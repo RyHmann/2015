7b9a611a8fec613963ed10aeed93ffbd1cd9e438
Ryan Heitmann <ryan.heitmann@gmail.com>
2021-06-11 11:56:47 +1200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -11,37 +11,8 @@ namespace _2015
     {
         static void Main(string[] args)
         {
-            /*
-            To run an excercise, create a new object instance of the excercise class, and run the objects .Run() method
 
-            For example, to run Day One's solution:
-
-            var DayOneSolution = new Day1();
-            DayOneSolution.Run();
-
-            */
-
-            // Day 5
-            /*
-            int niceWordCounter = 0;
-            string data = "C:/Repos/excercises/Advent of Code/data/2015/05.txt";
-            var NiceChecker = new Day5();
-            
-            foreach (string line in File.ReadLines(data))
-            {
-                bool isNice = NiceChecker.IsNicePartOne(line);
-                if (isNice)
-                {
-                    niceWordCounter++;
-                }
-            }
-            Console.WriteLine($"Total number of nice words: {niceWordCounter}");
-            */
-            string stringToCheck = "abcdefeghi";
-
-            Regex twoCharsCheck = new Regex(@"(\w)\1(\w)");
-            bool twoCharsInARowOccurs = twoCharsCheck.IsMatch(stringToCheck);
-            Console.WriteLine($"Sandwiched: {twoCharsInARowOccurs}");
+            Console.WriteLine("Hello World!");
 
         }
     }
